a9f4348a8977a7e7aa16c04942d73b18160c7588
HarryDang <danghuuphuc001@gmail.com>
2025-08-25 15:20:15 +0700

    feat(preview-image): preview image

---
+++ src/components/custom/ImagePreview.vue
@@ -0,0 +1,107 @@
+<template>
+  <Dialog :open="isOpen" @update:open="isOpen = $event">
+    <DialogTrigger as-child>
+      <slot v-if="slots.default" />
+      <img
+        v-else
+        :src="currentImage"
+        :alt="currentAlt"
+        :class="cn('h-full w-full object-cover', classMain)"
+      />
+    </DialogTrigger>
+    <DialogContent
+      class="flex min-h-1/2 max-w-full min-w-[80vw] items-center justify-center"
+    >
+      <!-- <DialogHeader>
+        <DialogTitle>Image Preview</DialogTitle>
+        <DialogDescription> This is an image preview. </DialogDescription>
+      </DialogHeader> -->
+      <div class="flex w-full items-center justify-between">
+        <div
+          @click.prevent="prevImage()"
+          class="flex cursor-pointer items-center justify-center rounded-full bg-slate-200 p-2 transition hover:scale-105 hover:bg-slate-300"
+        >
+          <ChevronLeftIcon class="size-8" />
+        </div>
+
+        <motion.img
+          :key="changeImage"
+          :src="changeImage"
+          :alt="currentAlt"
+          :class="cn('size-[600px] rounded-2xl object-cover', classImage)"
+          :initial="{ opacity: 0 }"
+          :animate="{ opacity: 1 }"
+          :exit="{ opacity: 0 }"
+          :transition="{ duration: 0.3, stiffness: 100, damping: 10 }"
+        />
+
+        <div
+          @click.prevent="nextImage()"
+          class="flex cursor-pointer items-center justify-center rounded-full bg-slate-200 p-2 transition hover:scale-105 hover:bg-slate-300"
+        >
+          <ChevronRightIcon class="size-8" />
+        </div>
+      </div>
+    </DialogContent>
+    <!-- <DialogFooter>
+      <Button>Close</Button>
+    </DialogFooter> -->
+  </Dialog>
+</template>
+
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { cn } from "@/lib/utils";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-vue-next";
+import { motion } from "motion-v";
+import { computed, ref, useSlots, type HTMLAttributes } from "vue";
+
+interface Props {
+  //* the main image should be the first image in the array
+  images: string[];
+
+  //* the alt text should be the first alt text in the array
+  alt: string[];
+  classMain?: HTMLAttributes["class"];
+  classImage?: HTMLAttributes["class"];
+}
+
+const slots = useSlots();
+
+const props = withDefaults(defineProps<Props>(), {
+  alt: () => ["previewing image"],
+  classMain: "",
+  classImage: "",
+});
+
+const currentIndex = ref(0);
+
+const currentImage = ref(props.images[0]);
+
+const changeImage = computed(() => props.images[currentIndex.value]);
+const currentAlt = computed(() => props.alt[currentIndex.value] ?? "preview");
+
+// next / prev without double increment
+const nextImage = () => {
+  currentIndex.value =
+    currentIndex.value >= props.images.length - 1 ? 0 : currentIndex.value + 1;
+};
+
+const prevImage = () => {
+  currentIndex.value =
+    currentIndex.value <= 0 ? props.images.length - 1 : currentIndex.value - 1;
+
+  console.log(currentIndex.value);
+};
+
+const isOpen = ref(false);
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,12 +9,12 @@
         </VideoText>
       </div>
 
-      <AnimatePresence mode="popLayout" class="">
+      <AnimatePresence mode="popLayout">
         <motion.img
           :key="activeIndex"
           :src="images[activeIndex].src"
           alt="home-bg"
-          class="absolute right-1/2 left-1/2 col-span-2 mx-auto -translate-x-1/3 -translate-y-1/3 object-cover"
+          class="absolute right-1/2 left-1/2 z-10 col-span-2 mx-auto hidden h-[1000px] w-[900px] -translate-x-1/3 -translate-y-1/3 object-cover transition-all duration-300 lg:block"
           :initial="{
             opacity: 0,
           }"
@@ -25,26 +25,29 @@
             opacity: 0,
           }"
           :transition="{
+            duration: 0.5,
             stiffness: 100,
             damping: 10,
-            duration: 0.3,
           }"
-          :whileInView="{ opacity: 1 }"
         />
       </AnimatePresence>
     </div>
-    <div class="bg-sage flex justify-between rounded-2xl p-6">
-      <div class="flex min-w-[500px] flex-col">
-        <h2 class="text-dark-text mb-4 text-5xl font-semibold md:text-6xl">
-          Elevate Your Space with Timeless,<br />
-          Elegant, and Modern <br />
+    <div
+      class="bg-sage flex flex-col items-center justify-between rounded-2xl p-6 lg:items-start 2xl:flex-row"
+    >
+      <div class="flex w-full flex-col lg:min-w-[500px]">
+        <h2
+          class="text-dark-text mb-4 w-auto text-xl font-semibold text-wrap whitespace-normal md:text-2xl xl:text-4xl 2xl:text-5xl"
+        >
+          Elevate Your Space with Timeless,<br class="hidden lg:block" />
+          Elegant, and Modern <br class="hidden lg:block" />
           Furniture Designs
         </h2>
-        <p class="text-dark-text mb-6 max-w-sm text-2xl">
+        <span class="text-dark-text mb-6 max-w-sm text-base xl:text-2xl">
           Explore our latest collections and find the perfect piece to
           complement <br />
           your space.
-        </p>
+        </span>
         <button
           class="bg-dark hover:bg-olive inline-flex max-w-36 items-center gap-2 rounded-full px-6 py-3 font-medium text-white transition"
         >
@@ -58,20 +61,24 @@
               :key="i"
               @click="setActive(i)"
               class="max-w-5 cursor-pointer rounded-full p-4 transition"
-              :class="i === activeIndex ? 'scale-110 bg-red-600' : 'bg-red-400'"
+              :class="
+                i === activeIndex
+                  ? `${images[i].color} scale-110`
+                  : 'bg-stone-400'
+              "
             />
           </div>
         </div>
       </div>
 
-      <div class="flex flex-col items-center gap-4 pr-10">
+      <!-- <div class="flex flex-col items-center gap-4 pr-0 2xl:pr-5">
         <AnimatePresence mode="popLayout">
           <motion.img
             v-for="(image, index) in images[activeIndex].childSrc"
             :src="image"
             :key="image + index"
             alt="home-bg"
-            class="h-[350px] w-[350px] object-cover"
+            class="h-[400px] w-[400px] object-cover transition-all duration-300 lg:h-[350px] lg:w-[350px] xl:h-[350px] xl:w-[350px]"
             :initial="{
               opacity: 0,
             }"
@@ -87,17 +94,29 @@
             layout
           />
         </AnimatePresence>
+      </div> -->
 
-        <!-- <img
-          src="/src/assets/chair.png"
-          alt="home-bg"
-          class="h-[350px] w-[350px] object-cover"
-        />
-        <img
-          src="/src/assets/chair.png"
-          alt="home-bg"
-          class="h-[350px] w-[350px] object-cover"
-        /> -->
+      <div class="flex flex-col items-center gap-4 pr-0 2xl:pr-5">
+        <AnimatePresence mode="popLayout">
+          <ImagePreview
+            v-for="(image, index) in images[activeIndex].childSrc"
+            :key="image + index"
+            :images="images[activeIndex].childSrc"
+            :alt="[`Image ${index + 1}`]"
+          >
+            <!-- slot = trigger content -->
+            <motion.img
+              :src="image"
+              :alt="`Preview ${index + 1}`"
+              class="h-[400px] w-[400px] object-cover transition-all duration-300 lg:h-[350px] lg:w-[350px] xl:h-[350px] xl:w-[350px]"
+              :initial="{ opacity: 0 }"
+              :animate="{ opacity: 1 }"
+              :exit="{ opacity: 0 }"
+              :transition="{ duration: 1 }"
+              layout
+            />
+          </ImagePreview>
+        </AnimatePresence>
       </div>
     </div>
   </div>
@@ -107,15 +126,18 @@
 import { VideoText } from "@/components/ui/video-text";
 import { ref } from "vue";
 import { AnimatePresence, motion } from "motion-v";
+import ImagePreview from "@/components/custom/ImagePreview.vue";
 
 const images = [
   {
     src: "/src/assets/chair.png",
     childSrc: ["/src/assets/chair.png", "/src/assets/chair.png"],
+    color: "bg-[#6d7941]",
   },
   {
     src: "/src/assets/coma-chair.png",
     childSrc: ["/src/assets/coma-chair.png", "/src/assets/coma-chair.png"],
+    color: "bg-stone-600",
   },
 ];
 
@@ -126,6 +148,8 @@ const changeImage = () => {
     activeIndex.value++;
   }
 };
+
+setInterval(changeImage, 4000);
 
 const activeIndex = ref(0);
 
