949af35047f22d6665aab6cb9182ae2618f105c5
HarryDang <danghuuphuc001@gmail.com>
2025-09-10 17:23:49 +0700

    feat(about): head infor

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -1,12 +1,29 @@
-<template>
+<script setup lang="ts">
+import { MorphingText } from "../ui/morphing-text";
+import CompareSpace from "./components/CompareSpace.vue";
 
+const texts = [
+  "The Future is Now",
+  "Innovate Tomorrow",
+  "Shaping What’s Ahead",
+  "Dream. Build. Repeat.",
+  "Beyond Imagination",
+  "Tomorrow Starts Today",
+  "Vision Becomes Reality",
+  "Evolving with Time",
+  "Where Ideas Take Flight",
+];
+</script>
+
+<template>
+  <MorphingText
+    :texts="texts"
+    :morph-time="2"
+    :cool-down-time="4"
+    class="text-olive left-0 mx-0 max-w-full text-start [filter:url(#threshold)_blur(0px)] md:h-12"
+    class-text="text-4xl"
+  />
   <div>
-    <h1>About</h1>
+    <CompareSpace />
   </div>
 </template>
-
-
-
-<script setup lang="ts">
-
-</script>
---
+++ src/components/about/components/CompareSpace.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Compare } from "@/components/ui/compare";
+</script>
+
+<template>
+  <div
+    class="relative flex h-full w-full items-center overflow-hidden rounded-4xl p-[3px]"
+  >
+    <Compare
+      first-image="/without-furnitures.webp"
+      second-image="/with-furnitures.jpg"
+      first-content-class="object-cover object-left-top rounded-4xl overflow-hidden"
+      second-content-class="object-cover object-left-top rounded-4xl overflow-hidden"
+      class="z-10 h-[500px] w-full rounded-4xl"
+      slide-mode="drag"
+      first-image-alt="without furnitures"
+      second-image-alt="with furnitures"
+      :initialSliderPercentage="40"
+    >
+    </Compare>
+
+    <div
+      class="animate-rotate-border absolute inset-0 h-full w-full rounded-4xl bg-[conic-gradient(#57644e_20deg,transparent_120deg)]"
+    ></div>
+  </div>
+</template>
