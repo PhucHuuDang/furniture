e920c41646fc5882cf1c727710fd6c9aec813907
HarryDang <danghuuphuc001@gmail.com>
2025-09-15 16:26:26 +0700

    feat(title): title pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
+import { useTitle } from "@vueuse/core";
 import {
   createRouter,
   createWebHistory,
+  type NavigationGuardNext,
   type RouteLocationNormalizedLoaded,
 } from "vue-router";
 
@@ -11,6 +13,7 @@ interface RouteItem {
 
   meta?: {
     requiresAuth?: boolean;
+    title: string | ((route: RouteLocationNormalizedLoaded) => string);
 
     breadcrumb?: string | ((route: RouteLocationNormalizedLoaded) => string);
   };
@@ -21,28 +24,28 @@ const routes: RouteItem[] = [
     path: "/",
     name: "Home",
     component: () => import("@/views/Home.vue"),
-    meta: { breadcrumb: "Home" },
+    meta: { breadcrumb: "Home", title: "Home" },
   },
 
   {
     path: "/about",
     name: "About",
     component: () => import("@/components/about/About.vue"),
-    meta: { breadcrumb: "About" },
+    meta: { breadcrumb: "About", title: "About" },
   },
 
   {
     path: "/products",
     name: "Products",
     component: () => import("@/components/products/Products.vue"),
-    meta: { breadcrumb: "Products" },
+    meta: { breadcrumb: "Products", title: "Products" },
   },
 
   {
     path: "/gallery",
     name: "Gallery",
     component: () => import("@/components/gallery/Gallery.vue"),
-    meta: { breadcrumb: "Gallery" },
+    meta: { breadcrumb: "Gallery", title: "Gallery" },
   },
 
   {
@@ -52,6 +55,9 @@ const routes: RouteItem[] = [
     meta: {
       breadcrumb: (route: any) =>
         decodeURIComponent(route.params.productName as string),
+
+      title: (route: any) =>
+        decodeURIComponent(route.params.productName as string),
     },
   },
   {
@@ -59,11 +65,26 @@ const routes: RouteItem[] = [
     path: "/:pathMatch(.*)*",
     name: "NotFound",
     component: () => import("@/views/NotFound.vue"),
+    meta: { breadcrumb: "NotFound", title: "NotFound" },
   },
 ];
 
-export const router = createRouter({
+const router = createRouter({
   history: createWebHistory("/"),
 
   routes,
 });
+
+router.beforeEach(
+  (
+    to: RouteLocationNormalizedLoaded,
+    from: RouteLocationNormalizedLoaded,
+    next: NavigationGuardNext,
+  ) => {
+    document.title =
+      (to.meta.title as string).concat(" | Hue") || "Default Title";
+    next();
+  },
+);
+
+export { router };
